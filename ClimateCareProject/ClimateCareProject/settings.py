76bf11d3d2d9ea7531387e36b1783349a41315c1
Mauricio Correa <mdcorreah@eafit.edu.co>
2023-05-24 01:39:53 -0500

    Cambio del host

--- ClimateCareProject/ClimateCareProject/settings.py
+++ ClimateCareProject/ClimateCareProject/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2194bb90654b5135775f8f495eef0a09de7316db74f58785e8605774f69c624
-size 4982
+oid sha256:fc20100fae51e6b2ce178ff5de539d6c95f7e293eff346037eb79260e4264e6b
+size 4981
